d0ef6990c4f039b79fba94cc3381ea07bd451dbb
Yunus Emre <ynsmrkrkmzz@gmail.com>
2025-01-20 12:33:02 +0300

    20.1

--- src/containers/home/introduce/IntroduceHome.jsx
+++ src/containers/home/introduce/IntroduceHome.jsx
@@ -4,9 +4,8 @@ import styles from "./style.module.css";
 export const IntroduceHome = () => {
   return (
     <div className={styles.container}>
-      <header className={styles.header}></header>
+      <header className={styles.header}>(tanışma)</header>
       <main className={styles.main}></main>
-      <footer className={styles.footer}></footer>
     </div>
   );
 };
