74c81e24d6910c43a75aebe1dd1aee75c59b0075
Yunus Emre <ynsmrkrkmzz@gmail.com>
2025-09-08 11:02:55 +0300

    8.9

--- src/containers/home/intro/IntroSec.jsx
+++ src/containers/home/intro/IntroSec.jsx
@@ -5,7 +5,7 @@ import Marquee from "react-double-marquee";
 const IntroSec = () => {
   const marqueeProps = {
     children: "minimalism,cubism,user(interface,experience),",
-    speed: 0.4,
+    speed: 0.25,
     direction: "left",
     childMargin: 15,
     scrollWhen: "always",
